74a6c29e0f713b2990ca6550fade9d79336b1878
SaiKiranMatta <saikirangoud.matta@gmail.com>
2025-02-08 15:34:50 +0530

    add shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <h1 className=" text-4xl">Hello world</h1>
+      <Button variant="outline">Button</Button>
     </>
   );
 }
